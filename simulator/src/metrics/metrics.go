b5ec663b40a9b5d09cffdef97b0dab4db701be2e
Oscar <oscareinarsson@hotmail.com>
2023-11-21 11:26:54 +0100

    updated struct to a map

--- simulator/src/metrics/metrics.go
+++ simulator/src/metrics/metrics.go
@@ -1,89 +1,51 @@
 package metrics
 
 import (
-	"bufio"
+	"errors"
 	"os"
-	"strconv"
-	"strings"
+
+	"github.com/gocarina/gocsv"
 )
 
 type Metric struct {
-	timestamp               int64
-	load1m                  float64
-	load5m                  float64
-	load15m                 float64
-	sys_mem_swap_total      int64
-	sys_mem_swap_free       int64
-	sys_mem_free            int64
-	sys_mem_cache           int64
-	sys_mem_buffered        int64
-	sys_mem_available       int64
-	sys_mem_total           int64
-	sys_fork_rate           float64
-	sys_interrupt_rate      float64
-	sys_context_switch_rate float64
-	sys_thermal             float64
-	disk_io_time            float64
-	disk_bytes_read         float64
-	disk_bytes_written      float64
-	disk_io_read            float64
-	disk_io_write           float64
-	cpu_io_wait             float64
-	cpu_system              float64
-	cpu_user                float64
-	server_up               int64
+	Timestamp               int64   `csv:"timestamp"`
+	Load1m                  float64 `csv:"load-1m"`
+	Load5m                  float64 `csv:"load-5m"`
+	Load15m                 float64 `csv:"load-15m"`
+	Sys_Mem_Swap_Total      int64   `csv:"sys-mem-swap-total"`
+	Sys_Mem_Swap_Free       int64   `csv:"sys-mem-swap-free"`
+	Sys_Mem_Free            int64   `csv:"sys-mem-free"`
+	Sys_Mem_Cache           int64   `csv:"sys-mem-cache"`
+	Sys_Mem_Buffered        int64   `csv:"sys-mem-buffered"`
+	Sys_Mem_Available       int64   `csv:"sys-mem-available"`
+	Sys_Mem_Total           int64   `csv:"sys-mem-total"`
+	Sys_Fork_Rate           float64 `csv:"sys-fork-rate"`
+	Sys_Interrupt_Rate      float64 `csv:"sys-interrupt-rate"`
+	Sys_Context_Switch_Rate float64 `csv:"sys-context-switch-rate"`
+	Sys_Thermal             float64 `csv:"sys-thermal"`
+	Disk_Io_Time            float64 `csv:"disk-io-time"`
+	Disk_Bytes_Read         float64 `csv:"disk-bytes-read"`
+	Disk_Bytes_Written      float64 `csv:"disk-bytes-written"`
+	Disk_Io_Read            float64 `csv:"disk-io-read"`
+	Disk_Io_Write           float64 `csv:"disk-io-write"`
+	Cpu_Io_Wait             float64 `csv:"cpu-iowait"`
+	Cpu_System              float64 `csv:"cpu-system"`
+	Cpu_User                float64 `csv:"cpu-user"`
+	Server_Up               int64   `csv:"server-up"`
 }
 
-func Test() string {
-	return "hello"
-}
-
-func ReadFromFile(fileName string) {
-	filePath := "../../../dataset/" + fileName
-	println(filePath)
+func ReadFromFile(filePath string) ([]*Metric, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
-		println("Something went wrong reading file: %s", fileName)
+		return nil, errors.New("file not found")
 	}
 	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	var serverMetrics []Metric
-	scanner.Scan()
 
-	//This should handle all the headers
-	for scanner.Scan() {
-		line := strings.Split(scanner.Text(), ",")
-		var metric Metric
-		var metricError [24]error
-		metric.timestamp, metricError[0] = strconv.ParseInt(line[0], 10, 64)
-		metric.load1m, metricError[1] = strconv.ParseFloat(line[1], 10)
-		metric.load5m, metricError[2] = strconv.ParseFloat(line[2], 10)
-		metric.load15m, metricError[3] = strconv.ParseFloat(line[3], 10)
-		metric.sys_mem_swap_total, metricError[4] = strconv.ParseInt(line[4], 10, 64)
-		metric.sys_mem_swap_free, metricError[5] = strconv.ParseInt(line[5], 10, 64)
-		metric.sys_mem_free, metricError[6] = strconv.ParseInt(line[6], 10, 64)
-		metric.sys_mem_cache, metricError[7] = strconv.ParseInt(line[7], 10, 64)
-		metric.sys_mem_buffered, metricError[8] = strconv.ParseInt(line[8], 10, 64)
-		metric.sys_mem_available, metricError[9] = strconv.ParseInt(line[9], 10, 64)
-		metric.sys_mem_total, metricError[10] = strconv.ParseInt(line[10], 10, 64)
-		metric.sys_fork_rate, metricError[11] = strconv.ParseFloat(line[11], 10)
-		metric.sys_interrupt_rate, metricError[12] = strconv.ParseFloat(line[12], 10)
-		metric.sys_context_switch_rate, metricError[13] = strconv.ParseFloat(line[13], 10)
-		metric.sys_thermal, metricError[14] = strconv.ParseFloat(line[14], 10)
-		metric.disk_io_time, metricError[15] = strconv.ParseFloat(line[15], 10)
-		metric.disk_bytes_read, metricError[16] = strconv.ParseFloat(line[16], 10)
-		metric.disk_bytes_written, metricError[17] = strconv.ParseFloat(line[17], 10)
-		metric.disk_io_read, metricError[18] = strconv.ParseFloat(line[18], 10)
-		metric.disk_io_write, metricError[19] = strconv.ParseFloat(line[19], 10)
-		metric.cpu_io_wait, metricError[20] = strconv.ParseFloat(line[20], 10)
-		metric.cpu_system, metricError[21] = strconv.ParseFloat(line[21], 10)
-		metric.cpu_user, metricError[22] = strconv.ParseFloat(line[22], 10)
-		metric.server_up, metricError[23] = strconv.ParseInt(line[23], 10, 64)
-		for i, e := range metricError {
-			if e != nil {
-				println("Error occured on metric %d", i)
-			}
-		}
-		serverMetrics = append(serverMetrics, metric)
+	metrics := []*Metric{}
+
+	if err := gocsv.UnmarshalFile(file, &metrics); err != nil {
+		panic(err)
 	}
+
+	return metrics, nil
 }
