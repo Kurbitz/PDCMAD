1d9bd2cada97be65da1f19f8bf9ee2ccc20a1875
Gustav Kånåhols <gustavkanahols@gmail.com>
2023-11-23 22:21:15 +0100

    Write metrics to database and remove old code

--- simulator/src/simba/simba.go
+++ simulator/src/simba/simba.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"pdc-mad/influxdbAPI"
 	"pdc-mad/metrics"
 	"sync"
 	"time"
@@ -11,39 +13,33 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	const numWorkers = 19
+	// FIXME: Read this from a parameter or .env file
 	folderPath := "../../../dataset/"
 	files, err := os.ReadDir(folderPath)
-	var systemMetrics = []*metrics.SystemMetric{}
 	if err != nil {
 		log.Fatal(err)
 	}
-	filePathChan := make(chan string, len(files))
+
 	startTime := time.Now()
+
 	for _, file := range files {
 		if file.IsDir() || filepath.Ext(file.Name()) != ".csv" {
 			continue
 		}
 		filePath := folderPath + file.Name()
-		filePathChan <- filePath
 		wg.Add(1)
-		go func(filePathChan <-chan string) {
+		go func(filePath string) {
 			defer wg.Done()
-			for filepath := range filePathChan {
-				metric, _ := metrics.ReadFromFile(filepath)
-				systemMetrics = append(systemMetrics, metric)
-			}
-		}(filePathChan)
+			// Remove the .csv from the file name
+			// FIXME: Use better ID
+			id := filepath.Base(filePath)[:len(filepath.Base(filePath))-len(filepath.Ext(filePath))]
+			metric, _ := metrics.ReadFromFile(filePath, id)
+			influxdbAPI.WriteMetric(*metric)
+		}(filePath)
 
 	}
-	close(filePathChan)
+
 	wg.Wait()
-	for _, sm := range systemMetrics {
-		println(sm.Id)
-		println(sm.Metr[10].Server_Up)
-
-	}
 	elapsedTime := time.Since(startTime)
 	fmt.Printf("Time: %s", elapsedTime)
-
 }
