41c0cb7ad54715eae4edd60d92465817c482f498
Gustav Kånåhols <gustavkanahols@gmail.com>
2023-11-23 23:38:15 +0100

    Read config from .env file

--- simulator/simba/go.mod
+++ simulator/simba/go.mod
@@ -12,5 +12,6 @@ require (
 	github.com/deepmap/oapi-codegen v1.12.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.17.0 // indirect
 )
--- simulator/simba/simba.go
+++ simulator/simba/simba.go
@@ -6,13 +6,34 @@ import (
 	simba "pdc-mad/simba/internal"
 
 	"sync"
+
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	if _, tokenExists := os.LookupEnv("INFLUXDB_TOKEN"); !tokenExists {
+		panic("Missing variable in .env file: INFLUXDB_TOKEN")
+	}
+	if _, ipExists := os.LookupEnv("INFLUXDB_IP"); !ipExists {
+		panic("Missing variable in .env file: INFLUXDB_IP")
+	}
+	if _, portExists := os.LookupEnv("INFLUXDB_PORT"); !portExists {
+		panic("Missing variable in .env file: INFLUXDB_PORT")
+	}
+	if _, pathExists := os.LookupEnv("DATASET_PATH"); !pathExists {
+		panic("Missing variable in .env file: DATASET_PATH")
+	}
+}
 
 func main() {
 	var wg sync.WaitGroup
 	// FIXME: Read this from a parameter or .env file
-	folderPath := "../../../dataset/"
-	files, err := os.ReadDir(folderPath)
+	datasetPath := os.Getenv("DATASET_PATH")
+	files, err := os.ReadDir(datasetPath)
 	if err != nil {
 		panic(err)
 	}
@@ -21,7 +42,7 @@ func main() {
 		if file.IsDir() || filepath.Ext(file.Name()) != ".csv" {
 			continue
 		}
-		filePath := folderPath + file.Name()
+		filePath := datasetPath + file.Name()
 		wg.Add(1)
 		go func(filePath string) {
 			defer wg.Done()
