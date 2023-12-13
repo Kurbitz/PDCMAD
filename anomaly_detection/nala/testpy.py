815af99908ac4bb8a6699e289e2d2683374fcba5
segerstorm <75488618+segerstorm@users.noreply.github.com>
2023-12-13 09:31:47 +0100

    Added print for argument sent from Go testpy.py

--- anomaly_detection/nala/testpy.py
+++ anomaly_detection/nala/testpy.py
@@ -3,7 +3,8 @@ import sys
 def func():
     msg = "Hello from 'func', Go" 
     print(msg)
-
+    
+print(sys.argv[1]) # Prints argument on index 1 ie argument 2
 msg = "Hello Go"
 print(msg)
 func()
