765c0765968bfd04a85ece4010474c166dc4422c
Gustav Kånåhols <gustavkanahols@gmail.com>
2023-12-14 22:44:53 +0100

    Add method to write a single metric to the db

--- simulator/simba/internal/influxdbapi.go
+++ simulator/simba/internal/influxdbapi.go
@@ -50,3 +50,14 @@ func (api InfluxDBApi) WriteMetrics(m SystemMetric, gap time.Duration) error {
 	// FIXME: Handle errors
 	return nil
 }
+
+func (api InfluxDBApi) WriteMetric(m Metric, id string, timeStamp time.Time) error {
+	writeAPI := api.WriteAPI(org, bucket)
+
+	m.Timestamp = timeStamp.Unix()
+	p := influxdb2.NewPoint(measurement, map[string]string{"host": id}, m.ToMap(), timeStamp)
+
+	writeAPI.WritePoint(p)
+
+	return nil
+}
