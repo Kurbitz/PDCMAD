31c13e8f0527a0e45ab3e1b0fbd6d12d3c04e8f4
Meeptard <meeptard@gmail.com>
2023-12-20 14:46:12 +0100

    Add getmetrics function with error handling and pivot query

--- internal/influxdbapi/influxdbapi.go
+++ internal/influxdbapi/influxdbapi.go
@@ -3,6 +3,7 @@ package influxdbapi
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"internal/system_metrics"
 	"log"
@@ -54,36 +55,45 @@ func (api InfluxDBApi) WriteMetrics(m system_metrics.SystemMetric, gap time.Dura
 }
 
 // TODO duration need to be checked before use
-func (api InfluxDBApi) GetMetrics(host, duration string) system_metrics.SystemMetric {
+func (api InfluxDBApi) GetMetrics(host, duration string) (system_metrics.SystemMetric, error) {
 	queryAPI := api.QueryAPI(ORG)
-	fmt.Println(host, duration)
 	query := fmt.Sprintf(`from(bucket: "%v") |> range(start: -%v) |> filter(fn: (r) => r._measurement == "%v") |> filter(fn: (r) => r["host"] == "%v") |> pivot(rowKey: ["_time"],columnKey: ["_field"], valueColumn: "_value")`, BUCKET, duration, MEASUREMENT, host)
 	result, err := queryAPI.Query(context.Background(), query)
-	var metrics []map[string]interface{}
-	if err == nil {
-		// Use Next() to iterate over query result lines
-		for result.Next() {
-			currentValue := result.Record().Values()
-			delete(currentValue, "_start")
-			delete(currentValue, "_stop")
-			delete(currentValue, "_time")
-			metrics = append(metrics, currentValue)
-		}
-
-		if result.Err() != nil {
-			fmt.Printf("Query error: %s\n", result.Err().Error())
-		}
-		parsed, err := json.Marshal(metrics)
-		if err != nil {
-			panic(err)
-		}
-		var parsedMetrics []*system_metrics.Metric
-		json.Unmarshal(parsed, &parsedMetrics)
-		return system_metrics.SystemMetric{Id: host, Metrics: parsedMetrics}
-	} else {
-		panic(err)
+	if err != nil {
+		log.Printf("Error performing query on bucket '%s': %s\n", BUCKET, err)
+		return system_metrics.SystemMetric{}, err
 	}
-	// Ensures background processes finishes
+	if result == nil {
+		log.Printf("Duration '%v' on bucket '%v' is empty", duration, BUCKET)
+		return system_metrics.SystemMetric{}, errors.New("Empty query result")
+	}
+	var metrics []map[string]interface{}
+	// Use Next() to iterate over query result lines
+	for result.Next() {
+		currentValue := result.Record().Values()
+		delete(currentValue, "_start")
+		delete(currentValue, "_stop")
+		delete(currentValue, "_time")
+		metrics = append(metrics, currentValue)
+	}
+
+	if result.Err() != nil {
+		log.Printf("Query error: %s\n", result.Err().Error())
+		return system_metrics.SystemMetric{}, result.Err()
+	}
+
+	parsed, err := json.Marshal(metrics)
+	if err != nil {
+		log.Printf("Error when encoding data to json: %v\n", err)
+		return system_metrics.SystemMetric{}, err
+	}
+	var parsedMetrics []*system_metrics.Metric
+	err = json.Unmarshal(parsed, &parsedMetrics)
+	if err != nil {
+		log.Printf("Error when decoding json to struct: %v\n", err)
+		return system_metrics.SystemMetric{}, err
+	}
+	return system_metrics.SystemMetric{Id: host, Metrics: parsedMetrics}, nil
 }
 
 // Deletes all the metrics contained in the bucket in the time interval
