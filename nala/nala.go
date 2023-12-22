c8d4cf96d6850dbee3708c5f152b459fc3382445
Gustav Kånåhols <gustavkanahols@gmail.com>
2023-12-22 10:06:43 +0100

    Use testpy.py as python smoke test
    This will be useful when building a docker image, to see if the python
    environment works as expected

--- nala/nala.go
+++ nala/nala.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 
 	"github.com/gin-gonic/gin"
@@ -21,21 +22,37 @@ func triggerDetection(ctx *gin.Context) {
 }
 
 // Runs "testyp.py" and prints the output
-func pyCall() {
-	//Sets Arguments to the command
-	cmd := exec.Command("python", "./testpy.py", "Hello Python")
+func pythonSmokeTest() {
+
+	log.Println("Running python smoke test...")
+	cmd := exec.Command("python", "./testpy.py", "Python is working!")
 	//executes command, listends to stdout, puts w/e into "out" var unless error
 	out, err := cmd.Output()
 	if err != nil {
 		log.Fatal(err) // Only gives exit 1 if error, use "cmd.Stderr = os.Stderr" (import os)
 	}
 	//Print, Need explicit typing or it prints an array with unicode numbers
-	fmt.Println(string(out))
+	log.Print(string(out))
+	log.Println("Python smoke test complete!")
 }
 
 func main() {
-	pyCall()
+	f, err := os.OpenFile("/var/log/nala.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	mw := io.MultiWriter(os.Stdout, f)
+
+	log.SetOutput(mw)
+
+	log.Println("Starting Nala...")
+	pythonSmokeTest()
+
+	// Test-write a log message to /var/log/nala.log
+
 	router := gin.Default()
+
 	router.GET("/nala/trigger", triggerDetection)
 	router.Run("0.0.0.0:8088")
 }
