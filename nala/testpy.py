029ab3e8e87103f290f26afc3577305f9bf01fa7
Gustav Kånåhols <gustavkanahols@gmail.com>
2023-12-23 13:16:59 +0100

    Test the imports we use in outliers.py

--- nala/testpy.py
+++ nala/testpy.py
@@ -1,4 +1,10 @@
 import sys
+import pandas
+from sklearn.ensemble import IsolationForest
+from datetime import datetime
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 
 print(sys.argv[1]) # Prints argument on index 1 ie argument 2
